b2278927691ffd788bb33f002a426c5a75b0741b
Matt Hanlon <mhanlon@thecodehub.ie>
2021-02-02 15:39:34 +0000

    Initial Commit

---
+++ QuestionBot-SwiftUI/QuestionBot_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionBot_SwiftUIApp.swift
+//  QuestionBot-SwiftUI
+//
+//  Created by Matthew Hanlon on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct QuestionBot_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
